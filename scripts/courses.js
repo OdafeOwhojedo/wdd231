4d5c7cd132286ab74037396888b3f2cd311480b7
OdafeOwhojedo <oowhojedo@byupathway.edu>
2026-01-09 04:39:08 +0100

    working

--- scripts/courses.js
+++ scripts/courses.js
@@ -1,8 +1,8 @@
 const courses = [
   { subject: 'CSE', number: 110, title: 'Introduction to Programming', credits: 2, completed: true },
   { subject: 'WDD', number: 130, title: 'Web Fundamentals', credits: 2, completed: true },
-  { subject: 'CSE', number: 111, title: 'Programming with Functions', credits: 2, completed: false },
-  { subject: 'CSE', number: 210, title: 'Programming with Classes', credits: 2, completed: false },
+  { subject: 'CSE', number: 111, title: 'Programming with Functions', credits: 2, completed: true },
+  { subject: 'CSE', number: 210, title: 'Programming with Classes', credits: 2, completed: true },
   { subject: 'WDD', number: 131, title: 'Dynamic Web Fundamentals', credits: 2, completed: true },
   { subject: 'WDD', number: 231, title: 'Frontend Web Development I', credits: 2, completed: false }
 ];
@@ -18,7 +18,10 @@ function displayCourses(list) {
   list.forEach(course => {
     const card = document.createElement("div");
     card.classList.add("course-card");
-    if (course.completed) card.classList.add("completed");
+    if (course.completed) {
+    card.classList.add("completed");
+    card.setAttribute("data-completed", "true");
+    }
 
     card.innerHTML = `
       <h3>${course.subject} ${course.number}</h3>
